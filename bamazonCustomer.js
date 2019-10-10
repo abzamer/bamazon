be362c25ac93d681e88068fb42138e37d4b65ef8
Angelica Zamer <angelica.zamer@gmail.com>
2019-10-09 23:47:02 -0500

    Need to make function only display ID, name, price

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -12,13 +12,46 @@ var connection = mysql.createConnection({
 connection.connect(function(err) {
     if(err) throw err;
     console.log("connected as id " + connection.threadId);
-   afterConnection();
+   displayInventory();
 })
 
-function afterConnection(){
-    connection.query("SELECT * FROM products", function(err,res){
+function displayInventory() {
+    connection.query("SELECT * FROM products", function(err, res){
         if(err) throw err;
-        console.log(res);
-        connection.end();
+        
+        var products = [];
+        for (var i = 0; i < products.length; i++){
+            console.log(
+                "Item ID: " + products[1].item_id + "\n",
+            );
+        }
+        // TODO: how to loop through an array of objects
+        // for(var i = 0; i < products.length; i++){
+        //     console.log(res[i])
+        // };
+        // console.log(res);
+        connection.end();        
     })
-}
+};
+
+//function which ask user ID of product & how many units 
+// function buySomething() {
+//     inquirer
+//     .prompt([
+//         name: "product",
+//         type: "input",
+//         message: "Please enter the ID of the item you would like to purchase.",
+//         validate: function(value){
+//             if(isNaN(value) === false) {
+//                 return true;
+//             }
+//             return false;
+//         }
+//     ]).then(function(answer){
+//         if(answer)
+//     })
+// }
+
+
+//check if the store has enough of the product
+
