a53245c374c15280487c5a9479a2103d0b0f3e6a
Angelica Zamer <angelica.zamer@gmail.com>
2019-10-10 20:49:54 -0500

    added comments explaining code

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,8 +1,10 @@
+//dependencies
 var mysql = require("mysql");
 var inquirer = require("inquirer");
 require("dotenv").config();
-//chalk
+var chalk = require("chalk");
 
+// create the connection information for the SQL database
 var connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
@@ -11,24 +13,27 @@ var connection = mysql.createConnection({
     database: "bamazon_db"
 });
 
-connection.connect(function(err) {
-    if(err) throw err;
+// connect to the mysql server and sql database
+connection.connect(function (err) {
+    if (err) throw err;
     console.log("connected as id " + connection.threadId);
-   displayInventory();
+    //function will run once connectio is made. user will see inventory
+    displayInventory();
 })
 
+//the function will display item ID, product, and cost per unit
+//this also calls the buySomething();
 function displayInventory() {
     var query = "SELECT * FROM products";
-    connection.query(query, function(err, res){
-        if(err) throw err;
-        console.log(res);
-        for(i = 0; i < res.length; i++){
+    connection.query(query, function (err, res) {
+        if (err) throw err;
+        // console.log(res);
+        for (i = 0; i < res.length; i++) {
             console.log(
-                res[i].item_id, 
+                res[i].item_id,
                 res[i].product_name,
-                res[i].price);
+                "$" + res[i].price);
         }
-     
         buySomething();
     })
 };
@@ -49,25 +54,27 @@ function buySomething() {
             }
         ]).then(function (answer) {
             var query = "SELECT * FROM products WHERE item_id = ?";
-            connection.query(query, [answer.product], function(err, res){
+            //this makes the query to database to match the product user selected, using item ID
+            connection.query(query, [answer.product], function (err, res) {
                 if (err) throw err;
-                for(i=0; i < res.length; i++){
-                    if(res[i].stock_quantity >= parseInt(answer.quantity)){
+                //for loop is used to UPDATE database if enough quantity exists
+                for (i = 0; i < res.length; i++) {
+                    if (res[i].stock_quantity >= parseInt(answer.quantity)) {
                         var query = "UPDATE products SET stock_quantity = ? WHERE item_id = ?";
                         var stock = res[i].stock_quantity - answer.quantity;
                         var cost = res[i].price * answer.quantity;
-                        connection.query(query, [stock, answer.product], function(err){
-                            if(err) throw err;
+                        connection.query(query, [stock, answer.product], function (err) {
+                            if (err) throw err;
                             console.log("Your total cost today is: $" + cost);
                         })
                     } else {
                         console.log("Our apologies. We do not have enough to fulfill your request. Try choosing another item.");
                     }
-                }connection.end();
-            } )
-    })
+                } connection.end();
+            })
+        })
 }
 
- 
+
 
 
